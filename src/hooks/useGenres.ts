839aab593a7154d5dd1bd6d62d52902881791d53
Beatriz Leal <beatrizleal0801@gmail.com>
2024-04-07 11:27:27 +0100

    refactoring

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
-import apiClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
+import APIClient from "../services/api-client";
 
 export interface Genre {
   id: number;
@@ -9,14 +8,13 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => {
-  return useQuery({
+const apiClient = new APIClient<Genre>("/genres");
+
+const useGenres = () =>
+  useQuery({
     queryKey: ["genres"],
-    queryFn: () =>
-      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24h,
     initialData: { count: genres.length, results: genres },
   });
-};
-
 export default useGenres;
