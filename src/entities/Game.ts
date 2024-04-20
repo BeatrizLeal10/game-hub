e45b59df89f64b4438b7521e475fbfc991f3f331
Beatriz Leal <beatrizleal0801@gmail.com>
2024-04-20 22:38:35 +0100

    build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,11 +1,15 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   slug: string;
   name: string;
   description_raw: string;
+  genres: Genre[];
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  publishers: Publisher[];
 }
